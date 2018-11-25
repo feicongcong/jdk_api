97a2836f3abe66391e210cfeb7ecbb0e23d40f9b
feicongcong <734820209@qq.com>
2018-11-25 20:12:35 +0800

    feicongcong

--- src/main/java/com/fcc/jdk8api/core/IOClass.java
+++ src/main/java/com/fcc/jdk8api/core/IOClass.java
@@ -1,5 +1,8 @@
 package com.fcc.jdk8api.core;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+
 import java.io.*;
 import java.net.URL;
 
@@ -12,11 +15,14 @@ public class IOClass {
     //所有输入流都是抽象类InputStream(字节输入流)或抽象类Reader(字符输入流)的子类
     //所有输出流都是抽象类OutputStream(字节输出流)或抽象类Writer(字符输出流)的子类
     public static void main(String[] args) {
-        new IOClass().test();
+        new IOClass().fileStream();
+//        new IOClass().byteStream();
+//        new IOClass().bufferStream();
     }
 
 
-    private void test(){
+
+    private void fileStream(){
         URL url = this.getClass().getClassLoader().getResource("");
         File file = new File(url.getPath()+"\\in.txt");
         String path = IOClass.class.getClassLoader().getResource("").getPath();
@@ -32,23 +38,30 @@ public class IOClass {
         }
         try {
             FileInputStream is = new FileInputStream(file);
-            File fileOut = new File("out.txt");
-            if (file.exists()) {
+            File fileOut = new File(url.getPath()+"\\out.txt");
+            if (fileOut.exists()) {
             }else{
                 try {
-                    file.createNewFile();
+                    fileOut.createNewFile();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             FileOutputStream os = new FileOutputStream(fileOut);
-            byte[] buffer=new byte[1];
-            while (is.read(buffer)!=-1){
-                os.write(buffer);
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            StringBuffer sb=new StringBuffer();
+            byte[] buffer=new byte[32];
+            int len=0;
+            while ((len=is.read(buffer))!=-1){
+                sb.append(new String(buffer,0,len));
+                bos.write(buffer,0,len);
             }
-
+            System.out.println(sb);
+            bos.flush();
+            bos.close();
             is.close();
             os.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,5 +70,52 @@ public class IOClass {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void byteStream(){
+        try {
+            byte[] bytes=new byte[]{97,97};
+            ByteInputStream is = new ByteInputStream(bytes,10);
+            ByteOutputStream os = new ByteOutputStream();
+            byte[] buffer=new byte[1024];
+            StringBuffer sb=new StringBuffer();
+            int len=0;
+            while ((len=is.read(buffer))!=-1){
+                sb.append(new String(buffer,0,len));
+                os.write(buffer);
+            }
+            System.out.println(new String(os.getBytes()));
+            is.close();
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void bufferStream(){
+        try {
+            byte[] bytes=new byte[]{97,97};
+            ByteInputStream is = new ByteInputStream(bytes,10);
+            BufferedInputStream bis=new BufferedInputStream(is);
+            ByteOutputStream os = new ByteOutputStream();
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            byte[] buffer=new byte[32];
+            StringBuffer sb=new StringBuffer();
+            int len=0;
+            while ((len=bis.read(buffer))!=-1){
+                sb.append(new String(buffer,0,len));
+                bos.write(buffer);
+            }
+            System.out.println(sb);
+            System.out.println(new String(os.getBytes()));
+            is.close();
+            bos.flush();
+            os.close();
+            bos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
